eeac2cec10045d4790acf50d49a20034ad98a5ca
garima verma <114860130+garimaver@users.noreply.github.com>
2024-06-04 18:21:51 +0530

    add something

--- harry-react2/src/components/News.jsx
+++ harry-react2/src/components/News.jsx
@@ -1,50 +1,73 @@
-import React, { Component } from "react";
-import NewsItem from "./NewsItem";
+import React, { Component } from 'react'
+import NewsItem from './NewsItem'
 
 export class News extends Component {
-  constructor() {
-    super();
-    console.log("hello i m construction");
-    this.state = {
-      articles: [],
-      loading: false,
-    };
-  }
 
-  async componentDidMount() {
-    console.log("cdm");
-    let url =
-      "https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=6a4432b566b04f6bb1e2b65c5b849960";
-    let data = await fetch(url);
-    let paresData = await data.json();
-    console.log(paresData);
-    this.setState({ articles: paresData.articles });
-  }
-  render() {
-    return (
-      <div className="container my-3">
-        <h2>News - Top Headlines</h2>
-        <div className="row">
-          {this.state.articles.map((element) => {
-            return (
-              <div className="col-md-4" key={element.url}>
-                <NewsItem
-                  title={element.title ? element.title : ""}
-                  description={element.description ? element.description : ""}
-                  imageUrl={element.urlToImage}
-                  newsUrl={element.url}
-                />
-              </div>
-            );
-          })}
-        </div>
-        <div className="container d-flex justify-content-between">
-        <button type="button" className="btn btn-dark">Previous</button>
-        <button  type="button" className="btn btn-dark ">Next</button>
-        </div>
-      </div>
-    );
-  }
+    constructor(){
+        super();
+        this.state = {
+            articles: [],
+            loading: false,
+            page:1
+        }
+    }
+
+    async componentDidMount(){ 
+        let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=dbe57b028aeb41e285a226a94865f7a7&page=1pageSize=20";
+        let data = await fetch(url);
+        let parsedData = await data.json()
+        console.log(parsedData); 
+        this.setState({articles: parsedData.articles, totalResults: parsedData.totalResults})
+    }
+
+     handlePrevClick = async ()=>{
+        console.log("Previous");
+        let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=dbe57b028aeb41e285a226a94865f7a7&page=${this.state.page - 1}&pageSize=20`;
+        let data = await fetch(url);
+        let parsedData = await data.json()
+        console.log(parsedData);  
+        this.setState({
+            page: this.state.page - 1,
+            articles: parsedData.articles
+        })
+
+    }
+    
+     handleNextClick = async ()=>{
+        console.log("Next"); 
+        if (this.state.page + 1 > Math.ceil(this.state.totalResults/20)){
+
+        }
+        else{
+            let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=dbe57b028aeb41e285a226a94865f7a7&page=${this.state.page + 1}&pageSize=20`;
+            let data = await fetch(url);
+            let parsedData = await data.json()
+            console.log(parsedData);  
+            this.setState({
+                page: this.state.page + 1,
+                articles: parsedData.articles
+            })
+    }
+    }
+
+    render() { 
+        return (
+            <div className="container my-3">
+                <h1 className='text-center'>NewsMonkey - Top Headlines</h1> 
+                <div className="row"> 
+                {this.state.articles.map((element)=>{
+                    return <div className="col-md-4" key={element.url}>
+                        <NewsItem title={element.title?element.title:""} description={element.description?element.description:""} imageUrl={element.urlToImage} newsUrl={element.url}/>
+                    </div> 
+                })} 
+                </div> 
+                <div className="container d-flex justify-content-between">
+                <button disabled={this.state.page<=1} type="button" className="btn btn-dark" onClick={this.handlePrevClick}> &larr; Previous</button>
+                <button type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
+                </div>
+            </div>
+        )
+    }
 }
 
-export default News;
+export default News
