5b79cd1851a3413b4e786285528ba9dd6d608125
garima verma <114860130+garimaver@users.noreply.github.com>
2024-06-04 14:06:19 +0530

    added sample

--- harry-react2/src/App.jsx
+++ harry-react2/src/App.jsx
@@ -1,11 +1,15 @@
 import React, { Component } from 'react'
+import Navbar from './components/Navbar'
+import News from './components/News'
+
 // 6a4432b566b04f6bb1e2b65c5b849960
 export default class App extends Component {
   c = "john"
   render() {
     return (
       <div>
-        <h1>hellowolrd {this.c}</h1>
+      <Navbar/>
+      <News/>
       </div>
     )
   }
