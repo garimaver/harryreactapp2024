d471aa409807f28e475dd6e19ed24ee1702d101d
garima verma <114860130+garimaver@users.noreply.github.com>
2024-06-06 12:31:36 +0530

    routing

--- harry-react2/src/App.jsx
+++ harry-react2/src/App.jsx
@@ -6,20 +6,20 @@ import { Route, Routes } from 'react-router-dom'
 export default class App extends Component {
 
   render() {
-    const country = "us"; // Define the country code
+    const country = "in"; // Define the country code
     const categories = ["general", "business", "entertainment", "science", "sports", "technology", "health"];
 
     return (
       <div>
         <Navbar/>
         <Routes>
-          <Route path='/' element={<News pageSize={5} country={country} category={categories[0]}/>} />
-          <Route path='/business' element={<News pageSize={5} country={country} category={categories[1]}/>} />
-          <Route path='/entertainment' element={<News pageSize={5} country={country} category={categories[2]}/>} />
-          <Route path='/science' element={<News pageSize={5} country={country} category={categories[3]}/>} />
-          <Route path='/sports' element={<News pageSize={5} country={country} category={categories[4]}/>} />
-          <Route path='/technology' element={<News pageSize={5} country={country} category={categories[5]}/>} />
-          <Route path='/health' element={<News pageSize={5} country={country} category={categories[6]}/>} />
+          <Route exact   path='/' element={<News pageSize={5} country={country} category={categories[0]}/>} />
+          <Route exact key="business" path='/business' element={<News pageSize={5} country={country} category={categories[1]}/>} />
+          <Route exact key="entertainment" path='/entertainment' element={<News pageSize={5} country={country} category={categories[2]}/>} />
+          <Route exact key="science" path='/science' element={<News pageSize={5} country={country} category={categories[3]}/>} />
+          <Route exact key="sports" path='/sports' element={<News pageSize={5} country={country} category={categories[4]}/>} />
+          <Route exact key="technology" path='/technology' element={<News pageSize={5} country={country} category={categories[5]}/>} />
+          <Route exact key="health" path='/health' element={<News pageSize={5} country={country} category={categories[6]}/>} />
         </Routes>
       </div>
     )
--- harry-react2/src/components/NewsItem.jsx
+++ harry-react2/src/components/NewsItem.jsx
@@ -1,17 +1,21 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom';
 
 export class NewsItem extends Component {
  
   render() {
-    let {title , description, imageUrl, newsUrl} = this.props;
+    let {title , description, imageUrl, newsUrl, author, date, source} = this.props;
     return (
       <div className='my-3'>
      <div className="card" style={{"width": "18rem"}}>
   <img src={!imageUrl? "https://www.hindustantimes.com/ht-img/img/2024/06/03/1600x900/PM-Narendra-Modi-in-Delhi-on-Wednesday---Vipin-Kum_1716441540763_1717405884111.jpg" : imageUrl} className="card-img-top" alt="..."/>
   <div className="card-body">
-    <h5 className="card-title">{title}</h5>
+    <h5 className="card-title">{title}  <span class="position-absolute top-0  translate-middle badge rounded-pill bg-danger" style={{left: '90%', zIndex:'1'}}>
+    {source}
+  </span></h5>
     <p className="card-text">{description}</p>
-    <a href={newsUrl} target='_blank' className="btn btn-sm btn-dark">Read more</a>
+    <p className='card-text'><small className='text-muted'>By {!author?"Unknown":author} on {new Date (date).toGMTString()}</small></p>
+    <Link to={newsUrl} target='_blank' className="btn btn-sm btn-dark">Read more</Link>
   </div>
 </div>
       </div>
