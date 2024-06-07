b970504fe7ce1f5104174066dd4ce10321f30c53
garima verma <114860130+garimaver@users.noreply.github.com>
2024-06-07 14:24:35 +0530

    update auth

--- client-inotebook/backend/routes/auth.js
+++ client-inotebook/backend/routes/auth.js
@@ -10,18 +10,18 @@ router.post('/createuser', [
     body('name', 'Enter a valid name').isLength({ min: 3 }),
     body('email', 'Enter a valid email').isEmail(),
     body('password', 'Password must be at least 5 characters').isLength({ min: 5 }),
-], (req, res) => {
+],async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
     }
-    User.create({
+   let user = await  User.create({
         name: req.body.name,
         password: req.body.password,
         email: req.body.email,
     }).then(user => res.json(user)).catch(err => {
         console.log(err);
-        return res.status(500).json({ error: 'Internal Server Error' });
+        return res.status(500).json({ error: 'Internal Server Error' , message : err.message});
     });
 });
 
