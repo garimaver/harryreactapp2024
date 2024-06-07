b7a5d253dbea7adafe7ea7571e0bf2cd56f2db4f
garima verma <114860130+garimaver@users.noreply.github.com>
2024-06-07 11:32:28 +0530

    add post method

--- client-inotebook/backend/routes/auth.js
+++ client-inotebook/backend/routes/auth.js
@@ -1,12 +1,15 @@
 const express = require("express");
+const User = require ('../models/User');
 const router = express.Router();
 
 // Middleware to parse JSON bodies
 router.use(express.json());
 
-router.get('/', (req, res) => {
+router.post('/', (req, res) => {
     console.log(req.body); // Corrected to log the request body
-    res.send("hi Garima");
+   const user = User(req.body);
+   user.save()
+    res.send(req.body);
 });
 
 module.exports = router;
