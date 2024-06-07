43964e1c373fa6036278cf303ba2cff00a184a3c
garima verma <114860130+garimaver@users.noreply.github.com>
2024-06-07 12:31:19 +0530

    creating frontend

--- client-inotebook/backend/routes/auth.js
+++ client-inotebook/backend/routes/auth.js
@@ -1,15 +1,28 @@
 const express = require("express");
-const User = require ('../models/User');
+const User = require('../models/User');
+const { body, validationResult } = require("express-validator");
 const router = express.Router();
 
 // Middleware to parse JSON bodies
 router.use(express.json());
 
-router.post('/', (req, res) => {
-    console.log(req.body); // Corrected to log the request body
-   const user = User(req.body);
-   user.save()
-    res.send(req.body);
+router.post('/', [
+    body('name', 'Enter a valid name').isLength({ min: 3 }),
+    body('email', 'Enter a valid email').isEmail(),
+    body('password', 'Password must be at least 5 characters').isLength({ min: 5 }),
+], (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    User.create({
+        name: req.body.name,
+        password: req.body.password,
+        email: req.body.email,
+    }).then(user => res.json(user)).catch(err => {
+        console.log(err);
+        return res.status(500).json({ error: 'Internal Server Error' });
+    });
 });
 
 module.exports = router;
