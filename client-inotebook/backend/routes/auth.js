bf19669be3820d30ea0945f7edc30d98e3c03f0b
garima verma <114860130+garimaver@users.noreply.github.com>
2024-06-09 09:43:58 +0530

    added auth validation

--- client-inotebook/backend/routes/auth.js
+++ client-inotebook/backend/routes/auth.js
@@ -15,7 +15,7 @@ router.post('/createuser', [
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
     }
-   let user = User.findOne({email : req.body.email})
+   let user = await User.findOne({email : req.body.email})
    if(user){
     return res.status(400).json({error: "sorry a user with this email already exists"})
    }
@@ -28,6 +28,7 @@ router.post('/createuser', [
     //     console.log(err);
     //     return res.status(500).json({ error: 'Internal Server Error' , message : err.message});
     // });
+    res.json({"Nice" : "nice"})
 });
 
 module.exports = router;
