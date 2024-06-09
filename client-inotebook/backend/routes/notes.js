9db5418d1979b856cd57a26b560a66e5406c5fb6
garima verma <114860130+garimaver@users.noreply.github.com>
2024-06-09 15:14:20 +0530

    add objeID

--- client-inotebook/backend/routes/notes.js
+++ client-inotebook/backend/routes/notes.js
@@ -1,34 +1,62 @@
 const express = require("express");
 const router = express.Router();
 const fetchuser = require("../middlewares/fetchuser");
+const validateObjectId = require("../middlewares/validateObjectId");
 const Note = require("../models/Note");
 const { body, validationResult } = require("express-validator");
 
 router.get('/fetchallnotes', fetchuser, async (req, res) => {
     try {
         const notes = await Note.find({ user: req.user.id });
-        res.json(notes); // Send the fetched notes as a JSON response
+        res.json(notes);
     } catch (error) {
         console.error(error.message);
         res.status(500).send("Internal server error");
     }
 });
 
-router.post('/addnotes', fetchuser,[
-    body('title', 'Enter a valid title').isLength({min : 3}),
+router.post('/addnotes', fetchuser, [
+    body('title', 'Enter a valid title').isLength({ min: 3 }),
     body('description', 'Description must be at least 5 characters').isLength({ min: 5 }),
-] , async (req, res) => {
-    const {title, description, tag} = req.body;
+], async (req, res) => {
+    const { title, description, tag } = req.body;
     try {
         const errors = validationResult(req);
-        if(!errors.isEmpty()){
-            return res.status(400).json({errors : errors.array()})
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
         }
-     const notes = new Note({
-        title, description, tag, user:req.user.id
-     })
-     const savedNote = await notes.save()
-        res.json(savedNote); // Send the fetched notes as a JSON response
+        const note = new Note({
+            title, description, tag, user: req.user.id
+        });
+        const savedNote = await note.save();
+        res.json(savedNote);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal server error");
+    }
+});
+
+router.put('/updatenotes/:id', fetchuser, validateObjectId, async (req, res) => {
+    const { title, description, tag } = req.body;
+
+    // Create a newNote object
+    const newNote = {};
+    if (title) { newNote.title = title; }
+    if (description) { newNote.description = description; }
+    if (tag) { newNote.tag = tag; }
+
+    try {
+        // Find the note to be updated and update it
+        let note = await Note.findById(req.params.id);
+        if (!note) { return res.status(404).send("Not found"); }
+
+        // Allow update only if user owns this note
+        if (note.user.toString() !== req.user.id) {
+            return res.status(401).send("Not Allowed");
+        }
+
+        note = await Note.findByIdAndUpdate(req.params.id, { $set: newNote }, { new: true });
+        res.json({ note });
     } catch (error) {
         console.error(error.message);
         res.status(500).send("Internal server error");
