4c6e2942cb75a075d97e49d52cb5884edbba951a
garima verma <114860130+garimaver@users.noreply.github.com>
2024-06-09 14:15:48 +0530

    add notes authentication

--- client-inotebook/backend/routes/notes.js
+++ client-inotebook/backend/routes/notes.js
@@ -2,11 +2,33 @@ const express = require("express");
 const router = express.Router();
 const fetchuser = require("../middlewares/fetchuser");
 const Note = require("../models/Note");
+const { body, validationResult } = require("express-validator");
 
 router.get('/fetchallnotes', fetchuser, async (req, res) => {
     try {
         const notes = await Note.find({ user: req.user.id });
         res.json(notes); // Send the fetched notes as a JSON response
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal server error");
+    }
+});
+
+router.post('/addnotes', fetchuser,[
+    body('title', 'Enter a valid title').isLength({min : 3}),
+    body('description', 'Description must be at least 5 characters').isLength({ min: 5 }),
+] , async (req, res) => {
+    const {title, description, tag} = req.body;
+    try {
+        const errors = validationResult(req);
+        if(!errors.isEmpty()){
+            return res.status(400).json({errors : errors.array()})
+        }
+     const notes = new Note({
+        title, description, tag, user:req.user.id
+     })
+     const savedNote = await notes.save()
+        res.json(savedNote); // Send the fetched notes as a JSON response
     } catch (error) {
         console.error(error.message);
         res.status(500).send("Internal server error");
