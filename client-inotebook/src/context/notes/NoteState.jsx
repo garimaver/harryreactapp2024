a4332b32ba5ed2c43458938d898c1311c9f48a0f
garima verma <114860130+garimaver@users.noreply.github.com>
2024-06-10 10:06:00 +0530

    write context

---
+++ client-inotebook/src/context/notes/NoteState.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import noteContext from "./noteContext"
+
+const NoteState = (props) => {
+  return (
+    <NoteState.provider>
+      {props.children}
+    </NoteState.provider>
+  )
+}
+
+export default NoteState
