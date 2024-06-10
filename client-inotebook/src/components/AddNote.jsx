a07754944a35c6d3756fe5f71841695a3272223e
garima verma <114860130+garimaver@users.noreply.github.com>
2024-06-10 12:42:16 +0530

    added delete functionality

--- client-inotebook/src/components/AddNote.jsx
+++ client-inotebook/src/components/AddNote.jsx
@@ -4,9 +4,10 @@ import noteContext from '../context/notes/noteContext'
 const AddNote = () => {
      const context = useContext(noteContext);
      const {addNote} = context;
-     const [note, setNote] = useState({title:"", description:"", tag:""})
-const handleClick = () => {
-addNote(note)
+     const [note, setNote] = useState({title:"", description:"", tag:"default"})
+const handleClick = (e) => {
+    e.preventDefault();
+addNote(note.title, note.description, note.tag);
 }
 
 const handleOnChange = (e) => {
@@ -28,7 +29,7 @@ const handleOnChange = (e) => {
   <input type="checkbox" className="form-check-input" id="exampleCheck1" onChange={handleOnChange}/>
   <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
 </div>
-<button type="submit" className="btn btn-primary" onClick={handleClick}>Submit</button>
+<button type="submit" className="btn btn-primary" onClick={handleClick}>Add Note</button>
 </form>
 
 </div>
--- client-inotebook/src/context/notes/NoteState.jsx
+++ client-inotebook/src/context/notes/NoteState.jsx
@@ -72,20 +72,22 @@ console.log("adding a new note")
 const note = {
   "_id": "66656ae1a61da0b73967472615",
   "user": "66655cd8b8b4efd3c95414748",
-  "title": "A day",
-  "description": "a day in my live if good",
-  "tag": "personal",
+  "title": title,
+  "description": description,
+  "tag": tag,
   "date": "2024-06-09T08:42:09.706Z",
   "__v": 0
 };
-setNotes(notes.push(note))
+setNotes(notes.concat(note))
   }
     //delete a note
-    const deleteNote = () => {
-    
+    const deleteNote = (id) => {
+     console.log("Deleting the note with id" + id);
+    const newNotes = notes.filter((note) => {return note._id !== id})
+     setNotes(newNotes)
     }
       //edit a note
-  const editNote = () => {
+  const editNote = (id, title, description, tag) => {
     
   }
   return (
